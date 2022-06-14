4ff41f8446dd75b0f989a152c844c6d01bbafdcc
gabusdev <gabusdev@outlook.com>
2022-06-14 17:53:07 -0400

    check point

---
+++ src/Application/UserNotifications/Queries/GetUnreadedNotificationsByUserId/GetUnreadedNotificationsByUserId.cs
@@ -0,0 +1,34 @@
+﻿using Application.Common.Persistence;
+using Application.Common.Specification;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.UserNotifications.Queries.GetUnreadedNotifications;
+
+public class GetUnreadedNotificationsByUserIdQuery : IQuery<List<NotificationDto>>
+{
+    public string UserId { get; set; } = null!;
+
+    public class GetUnreadedNotificationsByUserIdQueryHandler : IQueryHandler<GetUnreadedNotificationsByUserIdQuery, List<NotificationDto>>
+    {
+        private readonly IUnitOfWork _uow;
+
+        public GetUnreadedNotificationsByUserIdQueryHandler(IUnitOfWork uow)
+        {
+            _uow = uow;
+        }
+        
+        public async Task<List<NotificationDto>> Handle(GetUnreadedNotificationsByUserIdQuery request, CancellationToken cancellationToken)
+        {
+            throw new Exception();
+            /*var source = await _uow.UserNotifications.GetAsync();
+            source.First()
+            return*/
+        }
+    }
+}
+
+
---
+++ src/Application/UserNotifications/Queries/NotificationDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.UserNotifications.Queries
+{
+    public class NotificationDto
+    {
+        public string Id { get; set; } = null!;
+        public string Message { get; set; } = null!;
+        public DateTime? SendedAt { get; set; }
+        public DateTime? RecivedAt { get; set; }
+    }
+}
