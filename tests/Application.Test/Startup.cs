6438f4d0403d0aa714c183769ca5eedcb84daa09
gabusdev <gabusdev@outlook.com>
2022-06-24 10:42:39 -0400

    now not working

--- tests/Application.Test/Startup.cs
+++ tests/Application.Test/Startup.cs
@@ -2,7 +2,6 @@ using Infrastructure;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Respawn;
 
 namespace Application.IntegrationTest;
 
---
+++ tests/WebApi.Test/Notification/PermaNotifiacationTests.cs
@@ -0,0 +1,27 @@
+﻿using Application.PermaNotifications.Queries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi.Controllers.v2;
+using Xunit;
+
+namespace WebApi.IntegrationTest.Notification
+{
+    public class PermaNotifiacationTests
+    {
+        private readonly NotificationController _controller;
+        public PermaNotifiacationTests()
+        {
+            _controller = new NotificationController();
+        }
+
+        [Fact]
+        public async Task GetNotifiacationReturnsOk()
+        {
+            var result = await _controller.GetUnreadedNotifications("");
+            Assert.IsType<List<NotificationDto>>(result);
+        }
+    }
+}
